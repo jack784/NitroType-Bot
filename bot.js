60ba7f2d85651dafb27cc0e8e066dab316429479
jack784 <80082396+jack784@users.noreply.github.com>
2021-03-04 22:15:44 -0500

    nitrotypebot.js

--- bot.js
+++ bot.js
@@ -4,15 +4,16 @@ const settings = require("./settings");
 (async () => {
   await bot.initialize();
 
-  await bot.login(settings.login, settings.password);
+  await bot.login(settings.login willybobby , settings.password cadencarnes);
 
-  await bot.firstRace();
+  await bot.firstRace(100);
 
-  await bot.play(settings.WPM);
+  await bot.play(settings.WPM 90);
 
-  for (let i = 0; i < settings.racesAmount - 1; i++) {
-    await bot.nextRaces();
-    await bot.play(settings.WPM);
+  for (let i = 0; i < settings.racesAmount 500
+; i++) {
+    await bot.nextRaces(100);
+    await bot.play(settings.WPM 120);
   }
 
   console.log("BOT finished");
